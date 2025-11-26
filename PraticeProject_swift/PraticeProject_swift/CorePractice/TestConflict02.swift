d4e77155edaa4d70a85fc599eb3bedf2809f53d5
Abhinav Srivastav <srivastav398@gmail.com>
2025-11-26 16:50:48 +0530

    WIP: Commit for conflict

---
+++ PraticeProject_swift/PraticeProject_swift/CorePractice/TestConflict02.swift
@@ -0,0 +1,8 @@
+//
+//  TestConflict02.swift
+//  PraticeProject_swift
+//
+//  Created by KTSPL on 26/11/25.
+//
+
+import Foundation
---
+++ PraticeProject_swift/PraticeProject_swift/CorePractice/TestConflit01.swift
@@ -0,0 +1,24 @@
+//
+//  TestConflit01.swift
+//  PraticeProject_swift
+//
+//  Created by KTSPL on 26/11/25.
+//
+
+import Foundation
+
+
+class ModelAttendance{
+    
+    let name: String
+    let date: String
+    let status: String
+    let image: String
+    
+    init(name: String, date: String, status: String, image: String) {
+        self.name = name
+        self.date = date
+        self.status = status
+        self.image = image
+    }
+}
